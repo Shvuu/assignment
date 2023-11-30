8e7ed4cf31adafb6856d255ff2afdee87de52020
Shvuu <148572881+Shvuu@users.noreply.github.com>
2023-11-30 22:37:03 +0530

    Add files via upload
    JAVA script 1st assigment

---
+++ Intro to JS/BMI.js
@@ -0,0 +1,6 @@
+const heightincm = 165 ;
+const weightinkg = 65 ;
+const BMI = weightinkg/(heightincm*heightincm);
+console.log("height in cm is :",heightincm) ;
+console.log("weight in kg is :",weightinkg) ;
+console.log("BMI is :",BMI)
---
+++ Intro to JS/areaofcircle.js
@@ -0,0 +1,5 @@
+const radius = 10 ;
+const pie = 3.141 ;
+const area = pie * radius * radius ;
+console.log("radius of circle is:", radius);
+console.log("area of circle is:",area) ;
